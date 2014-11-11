46ab1ce6ca4ed4fb8c63bb8f7bcf48cb3feb8557
Dane Petersen <thegreatsunra@gmail.com>
2014-11-11 10:36:38 -0800

    Call testContent and display in ng-repeat

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,7 @@
 angular.module('angularPropellerApp')
   .controller('MainCtrl', function ($scope, contentFactory) {
     $scope.meaning = contentFactory.someMethod();
-    
+    $scope.testContent = contentFactory.getTestContent.query();
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
