95d8c0c91d25ee7b3fdba45e9f686b72291c152a
Dane Petersen <thegreatsunra@gmail.com>
2014-11-15 10:15:00 -0800

    Add rudimentary nested states for about state

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -22,6 +22,22 @@ angular
       .state('about', {
         url: '/about',
         templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        abstract: 'true'
+      })
+      .state('about.stuff', {
+        url: '/stuff',
+        templateUrl: 'partials/about/stuff.html',
+        controller: 'AboutCtrl'
+      })
+      .state('about.things', {
+        url: '/things',
+        templateUrl: 'partials/about/things.html',
+        controller: 'AboutCtrl'
+      })
+      .state('about.moreThings', {
+        url: '/more-things',
+        templateUrl: 'partials/about/moreThings.html',
         controller: 'AboutCtrl'
       })
       .state('contact', {
