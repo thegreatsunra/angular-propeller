5b0e9205e5686b74410ac4d119a0f796b9096c4f
Dane Petersen <thegreatsunra@gmail.com>
2014-11-16 21:25:50 -0800

    Clean up About child state routes

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -23,22 +23,19 @@ angular
         url: '/about',
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl',
-        abstract: 'true'
+        abstract: true
       })
       .state('about.stuff', {
         url: '/stuff',
-        templateUrl: 'partials/about/stuff.html',
-        controller: 'AboutCtrl'
+        templateUrl: 'partials/about/stuff.html'
       })
       .state('about.things', {
         url: '/things',
-        templateUrl: 'partials/about/things.html',
-        controller: 'AboutCtrl'
+        templateUrl: 'partials/about/things.html'
       })
       .state('about.moreThings', {
         url: '/more-things',
-        templateUrl: 'partials/about/moreThings.html',
-        controller: 'AboutCtrl'
+        templateUrl: 'partials/about/moreThings.html'
       })
       .state('contact', {
         url: '/contact',
