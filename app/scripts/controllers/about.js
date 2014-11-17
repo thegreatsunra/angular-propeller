b89a91188397734b53cebe3df685ebe164936ae5
Dane Petersen <thegreatsunra@gmail.com>
2014-11-16 21:27:34 -0800

    Add testOne and testTwo $resources to about.stuff state

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,7 +8,10 @@
  * Controller of the angularPropellerApp
  */
 angular.module('angularPropellerApp')
-  .controller('AboutCtrl', function ($scope) {
+  .controller('AboutCtrl', function ($scope, contentFactory) {
+    $scope.testOneContent = contentFactory.getTestOneContent.query();
+    $scope.testTwoContent = contentFactory.getTestTwoContent.query();
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
